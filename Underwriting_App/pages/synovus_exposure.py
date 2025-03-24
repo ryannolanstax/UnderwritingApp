0a4d1651aeda7649485a3af644a3645d37ca06eb
Ryan Nolan <137296701+ryannolanstax@users.noreply.github.com>
2025-03-24 14:01:14 -0400

    Create synovus_exposure.py

---
+++ Underwriting_App/pages/synovus_exposure.py
@@ -0,0 +1,48 @@
+# python3 -m streamlit run Underwriting_App/pages/calc_2025_synovus.py 
+#last Updated End of April
+
+import altair as alt
+import pandas as pd
+import seaborn as sns
+import streamlit as st
+import streamlit.components.v1 as components
+import base64
+import json
+import numpy as np
+import datetime
+from datetime import date, timedelta
+import io
+import matplotlib.pyplot as plt  
+
+
+st.set_page_config(page_title="Synovus Underwriting Calculator", page_icon="💾", layout="wide")
+
+
+st.title("Synovus Underwriting and Risk Calculator")
+st.markdown("Having Issues or Ideas to improve the APP? Reach out to Ryan Nolan")
+
+st.header('Exposure Fields')
+
+delayed = pd.read_csv('Underwriting_App/MCC & Business Models - MCC Ratings_Sales.csv')
+
+MCC = st.number_input("MCC", key='MCC', value=1711)
+
+CNP_DD = delayed.loc[delayed['MCC'] == MCC, ['CNP Delayed Delivery']].iloc[0, 0]
+CP_DD = delayed.loc[delayed['MCC'] == MCC, ['CP/ACH Delayed Delivery']].iloc[0, 0]
+
+NDX = (CNP_DD + CP_DD) / 2
+
+Annual_Volume = st.number_input("Annual Volume ($)", key="Annual_Volume", step=1)
+
+daily_vol = Annual_Volume / 365
+monthly_vol = Annual_Volume / 12
+
+step_size = 0.001
+
+Refund_Rate = st.number_input("Refund Rate Last 90 Days", key="Refund_Rate", value=0.05)
+Chargeback_Rate = st.number_input("Chargeback Rate Last 90 Days", key="Chargeback_Rate", value=0.005, step=step_size, format="%f")
+
+Total_Exposure = ((daily_vol * NDX) + (monthly_vol * Refund_Rate) + (monthly_vol * Chargeback_Rate))
+
+formatted_exposure = "${:,.0f}".format(Total_Exposure)
+st.write('The Final Estimated Synovus Exposure of the Customer is:', formatted_exposure)
